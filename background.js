f4bc7a1bd745b983bd2d59f9c87ad48f88e9cad3
Maliha Islam <maliha.rh@gmail.com>
2018-11-17 18:05:04 -0500

    comment out code

--- background.js
+++ background.js
@@ -5,16 +5,17 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
+//  chrome.storage.sync.set({color: '#3aa757'}, function() {
+//    console.log('The color is green.');
+//  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {schemes: ['https'], urlContains: 'order'},
-
+        pageUrl: {schemes: ['https'], urlContains: 'order'}
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
+
+console.log('blah');
