be387f00201b2315ab29be698058eab5dcb68330
Maliha Islam <maliha.rh@gmail.com>
2018-11-17 17:31:31 -0500

    update declarativeContent rule

--- background.js
+++ background.js
@@ -11,7 +11,8 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {schemes: ['https']}
+        pageUrl: {schemes: ['https'], urlContains: 'order'},
+
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
