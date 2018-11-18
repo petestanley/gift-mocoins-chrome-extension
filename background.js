6bbfb4c246de90b6cd6b2e7bf677a0592f71ab2d
maliha.islam <maliha.islam@points.com>
2018-11-17 20:37:53 -0500

    update declarativeContent conditions

--- background.js
+++ background.js
@@ -12,7 +12,10 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {schemes: ['https']}
+        pageUrl: {hostEquals: 'www.amazon.ca'}
+      }),
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'ca.dollarshaveclub.com'}
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
