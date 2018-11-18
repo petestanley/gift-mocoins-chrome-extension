54ea8dcb18bd73070350444cc6f8567406ad78a8
ekristina <panacejja@gmail.com>
2018-11-17 20:24:12 -0500

    refactor

--- payload.js
+++ payload.js
@@ -8,17 +8,12 @@ var currentUrl = window.location.href;
 
 if (currentUrl.includes('amazon')) {
     // Read total from Amazon
-    for (const tag of document.querySelectorAll("td")) {
-      if (tag.textContent.includes("Total")) {
-        var value = tag.nextElementSibling;
-        console.log(tag.nextElementSibling);
-        var value_content = value.textContent;
-        var total = parseFloat(value_content.substring(5,value_content.length));
-        var nearest_dol = Math.ceil(total);
-        let paymentAmount = parseInt((nearest_dol - total) * 100);
-      }
-    }
-} else if(currentUrl.includes('dollarshaveclub')){
+    var paymentAmount = document.querySelector("td.grand-total-price strong").textContent;
+    paymentAmount = paymentAmount.substring(5, paymentAmount.length);
+    paymentAmount = convertToMoCoins(paymentAmount);
+}
+
+else if(currentUrl.includes('dollarshaveclub')) {
     // Read total from Dollar Shave Club
     let paymentAmount = document.querySelector('.total .currency-units').textContent +
         '.' +
